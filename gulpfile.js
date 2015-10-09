dc99f2c27eb6b39ee2020a0e34bf82139bf3da2e
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2015-10-09 12:52:27 +0200

    add sidebar

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var
 gulp = require('gulp'),
-ts = require('gulp-typescript');
+ts = require('gulp-typescript'),
+less = require('gulp-less');
 
 gulp.task('setupApplicationJs', function() {
   return gulp.src('./src/server/**/*.js')
@@ -15,4 +16,16 @@ gulp.task('setupApplicationTs', ['setupApplicationJs'], function() {
 
 gulp.task('setupApplicationWatch', ['setupApplicationJs', 'setupApplicationTs'], function() {
   return gulp.watch('./src/server/**/*.{ts,js}', ['setupApplicationJs', 'setupApplicationTs']);
+});
+
+gulp.task('css', function () {
+  return gulp.src('./src/client/**/*.css')
+    .pipe(less())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('less', ['css'], function () {
+  return gulp.src('./src/client/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public'));
 });
