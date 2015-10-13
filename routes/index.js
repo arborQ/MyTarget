7daadfa1ae6e0b4bb8bf0eb334632b2fb46ca8b3
arbor <arbor@o2.pl>
2015-10-13 20:56:32 +0200

    env

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var nconf = require('nconf');
+
+nconf.env()
+     .file({ file: 'config.json', search: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', production : nconf.get("NODE_ENV") });
 });
 
 module.exports = router;
